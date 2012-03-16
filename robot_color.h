22a194ece54f51b211fc5f5fe92d89bc100129ab
Junchao Hua <juh30@pitt.edu>
2012-03-16 16:32:44 -0400

    added cvOr threshold image and get_square_difference() function

--- robot_color.h
+++ robot_color.h
@@ -8,8 +8,11 @@
 
 /* Pink */
 #define RC_PINK		5
-#define RC_PINK_LOW	cvScalar(0, 65, 47, 0)
-#define RC_PINK_HIGH	cvScalar(13, 255, 255, 0)
+#define RC_PINK_LOW_1	cvScalar(0, 67, 65, 0)	//i got better results with v = 65 rather than 47
+#define RC_PINK_HIGH_1	cvScalar(13, 255, 255, 0)
+
+#define RC_PINK_LOW_2	cvScalar(150, 67, 65, 0)
+#define RC_PINK_HIGH_2	cvScalar(190, 255, 255, 0)
 
 /* Yellow */
 #define RC_YELLOW	30
